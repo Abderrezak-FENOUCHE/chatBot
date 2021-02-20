64616437f338c95eb0a42307baa426eb1ec281ad
Abderrezak FENOUCHE <abderrezak.fenouche@edu.univ-paris13.fr>
2021-02-20 15:37:10 +0100

    capitalized port variable

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 initWebRoutes(app);
 
-let port = process.env.port || 8080
+let port = process.env.PORT || 8080
 
 app.listen(port,()=>{
     console.log(`App is runing on : http://localhost:${port}`);
