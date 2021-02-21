f6bdd26a44e522c1328dd2afd78730fae10e6a63
Abderrezak FENOUCHE <abderrezak.fenouche@edu.univ-paris13.fr>
2021-02-21 11:51:38 +0100

    modified attachment response

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -110,7 +110,7 @@ function handleMessage(sender_psid, received_message) {
     } else if (received_message.attachments) {
 
         response = {
-            "text": `Je ne sais pas traiter ce type de demande!`
+            "text": `Je ne sais pas traiter ce type de demande pour le moment!`
         }
 
     }
